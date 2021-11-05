9973e7b59ff3a632a0e8ccaf7f83b5376b7ac443
Akash Sengupta <akash.sengupta1997@gmail.com>
2021-11-05 14:38:44 +0000

    Update paths.py

--- configs/paths.py
+++ configs/paths.py
@@ -7,10 +7,6 @@ H36M_REGRESSOR = './model_files/J_regressor_h36m.npy'
 # ------------------- DensePose Files for Textured Rendering -------------------
 DP_UV_PROCESSED_FILE = './model_files/UV_Processed.mat'
 
-# ------------------------- Model Weights -------------------------
-HRNET_WEIGHTS = './model_files/pose_hrnet_w48_384x288.pth'
-POSE_SHAPE_NET_WEIGHTS = './model_files/poseMF_shapeGaussian_net_weights.tar'
-
 # ------------------------- Eval Datasets -------------------------
 PW3D_PATH = ''
 SSP3D_PATH = ''
